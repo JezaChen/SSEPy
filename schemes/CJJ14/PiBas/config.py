3e5d2b35524b0ef055e93bd257e38cb5bedf0fc5
JezaChen <jeza@vip.qq.com>
2022-03-13 09:15:12 +0800

    [CJJ+14] PiBas Completed

---
+++ schemes/CJJ14/PiBas/config.py
@@ -0,0 +1,63 @@
+# -*- coding:utf-8 _*-
+""" 
+LIB-SSE CODE
+@author: Jeza Chen
+@license: Apache Licence 
+@file: config.py
+@time: 2022/03/09
+@contact: jeza@vip.qq.com
+@site:  
+@software: PyCharm 
+@description: 
+"""
+
+import toolkit.config_manager
+import toolkit.prf
+import toolkit.prp
+import toolkit.symmetric_encryption
+from schemes.interface.config import SSEConfig
+
+PI_BAS_HEADER = b"\x93\x94Cash2014PiBas"
+
+# If the param of the specified length is not suffixed, the default is in bytes
+# The length parameter of the bit suffix is the length in bits
+
+DEFAULT_CONFIG = {
+    "param_lambda": 32,  # key size (bytes)
+    "prf_f_output_length": 32,
+
+    "prf_f": "HmacPRF",
+    "ske": "AES-CBC"
+}
+
+
+class PiBasConfig(SSEConfig):
+    __slots__ = [
+        "param_lambda",
+        "prf_f_output_length",
+
+        "prf_f",
+        "ske"
+    ]
+
+    def __init__(self, config_dict: dict):
+        super(PiBasConfig, self).__init__(config_dict)
+        self._parse_config(config_dict)
+
+    def _parse_config(self, config_dict: dict):
+        SSEConfig.check_param_exist(["param_lambda",
+                                     "prf_f_output_length",
+                                     "prf_f",
+                                     "ske"],
+                                    config_dict)
+
+        self.param_lambda = config_dict.get("param_lambda")
+        self.prf_f_output_length = config_dict.get("prf_f_output_length")
+
+        self.prf_f = toolkit.prf.get_prf_implementation(config_dict.get("prf_f", ""))(
+            key_length=self.param_lambda,
+            output_length=self.prf_f_output_length)
+
+        self.ske = toolkit.symmetric_encryption.get_symmetric_encryption_implementation(config_dict.get("ske", ""))(
+            key_length=self.param_lambda
+        )
---
+++ schemes/CJJ14/PiBas/structures.py
@@ -0,0 +1,101 @@
+# -*- coding:utf-8 _*-
+""" 
+LIB-SSE CODE
+@author: Jeza Chen
+@license: Apache Licence 
+@file: structures.py 
+@time: 2022/03/11
+@contact: jeza@vip.qq.com
+@site:  
+@software: PyCharm 
+@description: 
+"""
+import pickle
+
+from schemes.CJJ14.PiBas.config import PiBasConfig, PI_BAS_HEADER
+from schemes.interface.structures import SSEKey, SSEEncryptedDatabase, SSEToken, SSEResult
+
+
+class PiBasKey(SSEKey):
+    __slots__ = ["K"]
+
+    def __init__(self, K: bytes, config: PiBasConfig = None):
+        super(PiBasKey, self).__init__(config)
+        self.K = K
+
+    def serialize(self) -> bytes:
+        return self.K
+
+    @classmethod
+    def deserialize(cls, xbytes: bytes, config: PiBasConfig):
+        if len(xbytes) != config.param_k:
+            raise ValueError("The length of xbytes must be the same as the length of the parameter param_k.")
+
+        return cls(xbytes)
+
+
+class PiBasEncryptedDatabase(SSEEncryptedDatabase):
+    __slots__ = ["D"]  # dict D
+
+    def __init__(self, D: dict, config: PiBasConfig = None):
+        super(PiBasEncryptedDatabase, self).__init__(config)
+        self.D = D
+
+    @classmethod
+    def build_from_list(cls, kv_pairs: list, config: PiBasConfig = None):
+        kv_pairs.sort(key=lambda pair: pair[0])
+        D = {key: value for key, value in kv_pairs}
+        return cls(D, config)
+
+    def serialize(self) -> bytes:
+        data = PI_BAS_HEADER + pickle.dumps(self.D)
+        return data
+
+    @classmethod
+    def deserialize(cls, xbytes: bytes, config: PiBasConfig = None):
+        if xbytes[:len(PI_BAS_HEADER)] != PI_BAS_HEADER:
+            raise ValueError("Parse header error.")
+
+        data_bytes = xbytes[len(PI_BAS_HEADER):]
+        D = pickle.loads(data_bytes)
+        return cls(D)
+
+
+class PiBasToken(SSEToken):
+    __slots__ = ["K1", "K2"]  # K1, K2
+
+    def __init__(self, K1: bytes, K2: bytes, config: PiBasConfig = None):
+        super(PiBasToken, self).__init__(config)
+        self.K1 = K1
+        self.K2 = K2
+
+    def serialize(self) -> bytes:
+        return self.K1 + self.K2
+
+    @classmethod
+    def deserialize(cls, xbytes: bytes, config: PiBasConfig = None):
+        if len(xbytes) != 2 * config.param_k:
+            raise ValueError("The length of xbytes must be 2 times the length of the parameter param_k.")
+
+        K1, K2 = xbytes[:config.param_k], xbytes[config.param_k:]
+
+        return cls(K1, K2, config)
+
+
+class PiBasResult(SSEResult):
+    __slots__ = ["result"]
+
+    def __init__(self, result: list, config: PiBasConfig = None):
+        super(PiBasResult, self).__init__(config)
+        self.result = result
+
+    def serialize(self) -> bytes:
+        return pickle.dumps(self.result)
+
+    @classmethod
+    def deserialize(cls, xbytes: bytes, config: PiBasConfig = None):
+        result = pickle.loads(xbytes)
+        if not isinstance(result, list):
+            return ValueError("The data contained in xbytes is not a list.")
+
+        return cls(result, config)
