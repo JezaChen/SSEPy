b558f1bad2062c1b8c09ab36eef05194873d629c
JezaChen <jeza@vip.qq.com>
2022-03-30 14:43:33 +0800

    Refinement of notes for DP17 program implementation

--- schemes/DP17/Pi/construction.py
+++ schemes/DP17/Pi/construction.py
@@ -25,6 +25,11 @@ from toolkit.database_utils import get_total_size
 
 def _divide_to_buckets(array_size: int, bucket_size: int) -> (typing.List[int],
                                                               typing.List[typing.List]):
+    """
+    Pre-slices the array into buckets based on the size of the array and the size of the buckets,
+    returning the number of elements remaining in each bucket
+    and the keyword-file identifier pairs contained in each bucket.
+    """
     remaining_count_list = [min(bucket_size, array_size - begin_index) for begin_index in
                             range(0, array_size, bucket_size)]
     w_id_pair_list = [[] for _ in range(len(remaining_count_list))]
@@ -32,7 +37,7 @@ def _divide_to_buckets(array_size: int, bucket_size: int) -> (typing.List[int],
 
 
 class Pi(schemes.interface.inverted_index_sse.InvertedIndexSSE):
-    """Pi Construction described by Cash et al. [CT14]"""
+    """Pi Construction described by Demertzis et al. [DP17]"""
 
     def __init__(self, config: dict = DEFAULT_CONFIG):
         super(Pi, self).__init__()
@@ -40,6 +45,11 @@ class Pi(schemes.interface.inverted_index_sse.InvertedIndexSSE):
         pass
 
     def _find_adjacent_i(self, db_w_len: int, levels_list: typing.List[int]) -> int:
+        """
+        Find adjacent j and i in L such that
+        L·2j < |D(w)| ≤ L·2i (if i is the smallest level, we ignore the lower bound).
+        :return: only returns i
+        """
         lo, hi = 0, len(levels_list)
         while lo <= hi:
             mid = (lo + hi) >> 1
@@ -52,6 +62,11 @@ class Pi(schemes.interface.inverted_index_sse.InvertedIndexSSE):
     def _get_remaining_enough_space_buckets(self,
                                             chunk_size: int,
                                             remaining_list: typing.List[int]) -> typing.List[int]:
+        """
+        returns the set of buckets in Ai that have enough space for chunk c
+        :param: chunk_size the size of chunk c
+        :param: remaining_list space remaining for each element of the array Ai
+        """
         for bucket_index, bucket_remaining in enumerate(remaining_list):
             if bucket_remaining >= chunk_size:
                 yield bucket_index
@@ -78,8 +93,8 @@ class Pi(schemes.interface.inverted_index_sse.InvertedIndexSSE):
         # Initialize a hash table HT that can store up to N elements.
         HT = {}
 
-        level_to_remaining_count_list_map = {}  # A = {Ai : i ∈ L}
-        level_to_w_id_pair_list_map = {}
+        level_to_remaining_count_list_map = {}  # level -> remaining count in each bucket
+        level_to_w_id_pair_list_map = {}  # level -> (w, id) pairs in each bucket
 
         for i in levels:  # each evenly distributed level i ∈ L
             level_to_remaining_count_list_map[i], level_to_w_id_pair_list_map[i] = _divide_to_buckets(
@@ -89,14 +104,19 @@ class Pi(schemes.interface.inverted_index_sse.InvertedIndexSSE):
             # Find adjacent j and i in L such that L· 2j < |D(w)| ≤ L· 2i
             # (if i is the smallest level, we ignore the lower bound)
             i = self._find_adjacent_i(len(database[keyword]), levels)
+
+            # Split D(w) into a set C_w of chunks containing q_w chunks of size 2i and one chunk of size r_w < 2^i.
             Cw = toolkit.list_utils.chunks(database[keyword], 2 ** i)
             count = 0
             for c in Cw:
                 count += 1
+                # A is the set of buckets in Ai that have enough space for chunk
                 A = list(self._get_remaining_enough_space_buckets(2 ** i,
                                                                   level_to_remaining_count_list_map[i]))
+                # Pick one bucket a ∈ A (say Ai[x]) uniformly at random
                 x = random.choice(A)
                 for identifier in c:
+                    # store c in a at the first available position.
                     level_to_w_id_pair_list_map[i][x].append((keyword, identifier))
 
                     # HT.add(H(Fk1(w)||count), [i||x] ⊕ H(Fk2(w)||count))
@@ -140,6 +160,7 @@ class Pi(schemes.interface.inverted_index_sse.InvertedIndexSSE):
     def _Trap(self, K: PiKey, keyword: bytes) -> PiToken:
         """Trapdoor Generation Algorithm"""
         k1, k2, k3 = K.k1, K.k2, K.k3
+        # tag ← F(k1, w), vtag ← F(k2, w), etag ← F(k3, w)
         tag = self.config.prf_f(k1, keyword)
         vtag = self.config.prf_f(k2, keyword)
         etag = self.config.prf_f(k3, keyword)
@@ -152,8 +173,10 @@ class Pi(schemes.interface.inverted_index_sse.InvertedIndexSSE):
         result = set()
 
         for count in range(1, self.config.param_L + 1):  # for count = 1 to L do
+            # evalue ← HT.get(H(tag||count))
             evalue = HT.get(self.config.hash_h(tag + int_to_bytes(count)))
             if evalue is not None:
+                # [i, offset] ← evalue ⊕ H(vtag||count)
                 i_concat_offset = bytes_xor(evalue, self.config.hash_h(vtag + int_to_bytes(count)))
                 i_bytes, offset_bytes = i_concat_offset[:self.config.param_hash_h_digest_size // 2], \
                                         i_concat_offset[self.config.param_hash_h_digest_size // 2:]
