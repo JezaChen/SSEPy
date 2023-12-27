de2e0dccff003a1380a14414306cb50808cac9c5
JezaChen <jeza@vip.qq.com>
2023-12-27 12:07:28 +0800

    [Bug Fixed] Solve the error caused by requesting the same sid within a short period of time.
    This problem occurs when the socket of the previous connection is not yet closed, and a new connection arrives and is interrupted because it cannot be processed, causing the client to report a network error.

--- frontend/server/services/services_manager.py
+++ frontend/server/services/services_manager.py
@@ -14,6 +14,7 @@ import asyncio
 
 from websockets.legacy.server import WebSocketServerProtocol
 
+from frontend.server.services.comm import send_message
 from frontend.server.services.service import Service
 from toolkit.logger.logger import getSSELogger
 
@@ -22,23 +23,31 @@ logger = getSSELogger("sse_server")
 
 class ServicesManager:
     def __init__(self):
-        self.service_dict = {}
+        self._access_dict_lock = asyncio.Lock()
+        self._service_dict = {}
 
     async def create_service(self, sid: str, websocket: WebSocketServerProtocol):
         logger.info(f"A new request for service {sid} found, creating...")
-        if sid in self.service_dict:
-            await websocket.close()
-            reason = f"Service {sid} is already running..."
-            logger.error(reason)
-            raise ValueError(reason)
+        # initialize a service first
         service = Service(sid, websocket)
-        self.service_dict[sid] = service
-        asyncio.create_task(self.clean_service_when_close_connection(sid, websocket))
+
+        if sid in self._service_dict:
+            prev_server = self._service_dict[sid]
+            reason = f"Service {sid} is already running, we need to wait for the previous connection to close..."
+            logger.warning(reason)
+            service.send_message("control", reason.encode('utf8'))
+            await prev_server.wait_closed()  # wait for the previous socket to close
+
+        async with self._access_dict_lock:
+            self._service_dict[sid] = service
+        clean_task = asyncio.create_task(self.clean_service_when_close_connection(sid, websocket))
         await service.start()  # run forever! do not use asyncio.create_task
+        await clean_task
 
     async def clean_service_when_close_connection(self, sid: str, websocket: WebSocketServerProtocol):
         await websocket.wait_closed()
-        await asyncio.sleep(5)
-        self.service_dict[sid].close_service()
-        del self.service_dict[sid]
+        async with self._access_dict_lock:
+            await asyncio.sleep(5)
+            self._service_dict[sid].close_service()
+            del self._service_dict[sid]
         logger.info(f"Clean service {sid} successfully.")
